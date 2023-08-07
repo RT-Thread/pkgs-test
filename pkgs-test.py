6193b2336f8dceb0b69b88fdfd8cf0a68d4e6295
vacabun <maguotong66@gmail.com>
2023-08-07 19:40:08 +0800

    only check latest pkg and master rt-thread.

--- pkgs-test.py
+++ pkgs-test.py
@@ -778,11 +778,26 @@ class Check:
 
     def check_errors(self):
         error_num = 0
-        for _, rtthread_res in self.pkgs_res_dict['pkgs_res'].items():
-            for _, bsp_res in rtthread_res.items():
-                for _, pkg_res in bsp_res.items():
-                    if pkg_res['error']:
+        if 'master' not in self.pkgs_res_dict['pkgs_res']:
+            print('[error] can not find rt-thread master version.')
+        else:
+            for bsp_name, bsp_res in self.pkgs_res_dict['pkgs_res']['master'].items():
+                for pkg_name, pkg_res in bsp_res.items():
+                    check_counter = 0
+                    for version_res in pkg_res['versions']:
+                        if version_res['version'] == 'latest':
+                            check_counter = check_counter + 1
+                            print('check {pkg} {version} on {bsp}.'.format(pkg=pkg_name, version=version_res['version'], bsp=bsp_name))
+                            if version_res['res'] == 'Failure':
+                                error_num = error_num + 1
+                                print('[error] compile failure.')
+                            else:
+                                print('compile success.')
+                        else:
+                            print('check {pkg} {version} on {bsp}. but not is latest, pass.'.format(pkg=pkg_name, version=version_res['version'], bsp=bsp_name))
+                    if check_counter == 0:
                         error_num = error_num + 1
+                        print('[error] can not find latest version.')
         return error_num
 
 
